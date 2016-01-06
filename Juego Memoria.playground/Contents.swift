0d4d16ab35335f82fd7255be6d11eaa859ffa422
DavidGtzRT <david@radicaltesta.com>
2016-01-06 17:32:44 -0600

    Commit Final

--- Juego Memoria.playground/Contents.swift
+++ Juego Memoria.playground/Contents.swift
@@ -2,4 +2,24 @@
 
 import UIKit
 
-var str = "Prueba Git"
+var rango = 0...100;
+
+for numero in rango {
+    if numero % 5 == 0{
+        print("\(numero) Bingo!!!")
+    }
+    
+    if numero % 2 == 0{
+        print("\(numero) par!!!")
+    }
+    else{
+        print("\(numero) inpar!!!")
+    }
+    
+    if numero > 30 && numero <= 40 {
+        print("\(numero) Viva Swift!!!")
+    }
+    
+    
+}
+
